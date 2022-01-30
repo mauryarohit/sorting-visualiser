f3e843eb671d310862763d2b711435f24510bc75
Rohit Maurya <maurya.rohit704@gmail.com>
2022-01-30 12:07:57 +0530

    Adding basic bar chart, importing chartJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sorting-visualiser';
+  barChartOptions = {
+    type: 'bar',
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      tooltip: false
+    },
+    hover: {
+      mode: null
+    },
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      }
+    }
+  };
+  barChartLabels = ['2000', '2001', '2002', '2003', '2004', '2005'];
+  barChartLegend = false;
+  barChartData = [
+    {
+      data: [75, 49, 89, 31, 86, 35, 50],
+      backgroundColor: [
+        'red',
+        'blue',
+        'red',
+        'red',
+        'red',
+        'red',
+      ]
+    },
+  ];
 }
